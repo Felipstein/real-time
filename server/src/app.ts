9be4c8835cfa4489af5c0744a8c0b0d6e3c9a177
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-08-06 21:44:30 -0300

    feat: add websocket

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
+import http from 'node:http'
 import express from 'express'
+import { Server } from 'socket.io'
 import cors from 'cors'
 
 const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.json())
 app.use(cors())
 
-export { app }
+io.on('connect', () => {
+  console.log('Client connected')
+})
+
+export { app, server, io }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
-import { app } from './app'
+import { server } from './app'
 
 console.log('\x1b[2J\x1b[0f')
 
-app.listen(3333, () => console.info('Server is running.'))
+server.listen(3333, () => console.info('Server is running.'))
