27bde724506daa1d46656a45660d9aaaedf82dc7
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-08-06 22:31:02 -0300

    feat: add mouse event

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,13 +5,20 @@ import cors from 'cors'
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, { cors: { origin: '*' } })
+
+let connected = 0
 
 app.use(express.json())
 app.use(cors())
 
-io.on('connect', () => {
-  console.log('Client connected')
+io.on('connection', (socket) => {
+  connected += 1
+  console.log(`a user connected ${connected} times`)
+
+  socket.on('mouse@move', (a) => {
+    socket.emit('mouse@client', a)
+  })
 })
 
 export { app, server, io }
